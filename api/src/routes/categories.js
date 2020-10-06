14627021455173fc773415c8d8dc4a436c43a4c5
Cinthia Daira Pardos <cin@pop-os.localdomain>
2020-10-06 16:37:54 -0300

    corregido

--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -3,12 +3,7 @@ const { Categories } = require("../db.js");
 
 server.post("/products/category", (req, res, next) => {
   //falta completar atributos
-/*acá debería hacerse destructuring o añadir el nombre de la variable detrás del req.body.etc
-         seria algo asi como: 
-let category = req.body.category; 
-	o de lo contrario 
-let {category} = req.body*/
-  category = req.body;
+  const { category } = req.body;
   Categories.findOrCreate()
     .then((category) => {
       console.log(category);
@@ -33,16 +28,5 @@ server.delete("products/category/:id", (req, res, next) => {
       return res.status(400).send("Category not found!");
     });
 });
-//eliminar siempre código desechado comentado o especificar si se necesitará!
-
-/*server.put('/products/:idProducto/category/:idCategoria', (req, res, next) => {
-    const categoria =
-       
-    Product.findAll()
-		.then(products => {
-			res.send(products);
-		})
-		.catch(next);
-});*/
 
 module.exports = server;
