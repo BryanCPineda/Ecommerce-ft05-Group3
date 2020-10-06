ef56d1cca22bd615e9b8f692ac801af07129e4a6
larts85 <larts85@yahoo.com>
2020-10-06 12:31:39 -0300

    create/add product route done!, id attribute repaired!

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,12 +1,26 @@
 const server = require('express').Router();
 const { Product } = require('../db.js');
-
-server.get('/', (req, res, next) => {
-	Product.findAll()
-		.then(products => {
-			res.send(products);
+// POST /products
+// Controla que estén todos los campos requeridos, si no retorna un statos 400.
+// Si pudo crear el producto retorna el status 201 y retorna la información del producto.
+server.post('/', (req, res, next) => {
+	const {name, description, price, stock} = req.body;
+	Product.findOrCreate({
+		where:{
+				name: name,
+				description: description,
+				price: price,
+				stock: stock,
+				image: 'don\'t forget to make it'
+		}
+	})
+	.then(product => {
+		console.log('PROMISE', product);
+			res.status(201).send(product);
 		})
-		.catch(next);
+	.catch(()=>{
+		return res.status(400).send('Product not created!');
+	})
 });
 
 module.exports = server;
