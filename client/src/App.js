baa32636f67f06a863eabaf8baff2f2c1dd65ced
Matias Funes <matiasfunes94@gmail.com>
2020-10-07 20:09:24 -0300

    S9-Crear-Formulario-para-creación-de-Categorías

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./App.css";
-import logo from './logo.svg';
 
 
 import SearchBar from './components/SearchBar/SearchBar.jsx';
 
 
-
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomeScreen from "./components/HomeScreen";
+import FormCategories from './components/FormCategories/FormCategories';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
@@ -23,6 +21,7 @@ function App() {
         {"aca va la navbar"}
         <Switch>
           <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/admin/createCategories" component={FormCategories} />
         </Switch>
         {"aca va el footer"}
       </Router>
