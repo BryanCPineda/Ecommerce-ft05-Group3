803ef1bb42e8e5cd7d33d635b0ef8e6689a46bbc
Santiago Barrios <santiagobarrios91@gmail.com>
2020-10-07 11:10:22 -0300

    cambios en rutas, añadiendo categoriesRouter, y modificando las rutas para hacerlas funcionales

--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -1,45 +1,45 @@
 const server = require("express").Router();
 const { Categories } = require("../db.js");
 
-server.post("/products/category", (req, res, next) => {
+server.post("/", (req, res, next) => {
   //falta completar atributos
-  const { category } = req.body;
-  Categories.findOrCreate()
+  const { name, description } = req.body;
+  Categories.findOrCreate({
+    where: {name:name, description:description}
+  })
     .then((category) => {
       console.log(category);
       res.status(201).json(category);
     })
-    .catch(next)
     .catch(() => {
       return res.status(400).send("Category not created!");
     });
 });
 
-server.delete("products/category/:id", (req, res, next) => {
+server.delete("/:id", (req, res, next) => {
   const { id } = req.params;
   Categories.destroy({
     where: { id: id },
-  })
-    .then(() => {
-      res.send("Category deleted");
+  }, /* {force:true} */) 
+    .then((resultado) => {
+      if(resultado){
+       return res.send("Category deleted")
+      }return res.status(400).send("Category not found!");
     })
-    .catch(next)
-    .catch(() => {
-      return res.status(400).send("Category not found!");
-    });
 });
 
-server.put('/products/category/:id', (req, res, next) => {
+server.put('/:id', (req, res, next) => {
   const {id} = req.params;
    const {name, description}= req.body /* <--- elemento del body que utilizaremos para el update */
   Categories.update(
      {name:name, description:description}, /* <----los atributos que queremos updatear */
     {where:{id:id}}
-  ).then(()=>{
-    res.status(202).send('Element updated')
-  }).catch(() => {
-    return res.status(400).send("Category not found!");
-  });
+  ).then((valor)=>{
+    const resultado=valor[0]
+    if(resultado){
+      return res.status(202).send('Element updated')
+     }return res.status(400).send("Category not found!")   
+  })
 });
 
 module.exports = server;
