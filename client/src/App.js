21b3166347169a74d4f085d9fb5cc04575f902c5
dgtech369 <lires.matiash@gmail.com>
2020-10-07 04:28:02 -0300

    App.js Resolved

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchBar from './components/SearchBar/SearchBar.jsx';
-import CarouselBootstrap from "./components/Carousel/Carousel.jsx"
+
 
 function App() {
   return (
     <div>
       <SearchBar />
-      <CarouselBootstrap />
+      
     </div>
   );
 }
