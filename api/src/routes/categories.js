402905a8ccf495e593ae13d69b90021b1d0a2460
Cinthia Daira Pardos <cin@pop-os.localdomain>
2020-10-08 01:07:46 -0300

    s17

--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -1,7 +1,7 @@
 const server = require("express").Router();
 const { Categories } = require("../db.js");
 
-server.post("/products/category", (req, res, next) => {
+server.post("/", (req, res, next) => {
   //falta completar atributos
   const { category } = req.body;
   Categories.findOrCreate()
@@ -9,13 +9,12 @@ server.post("/products/category", (req, res, next) => {
       console.log(category);
       res.status(201).json(category);
     })
-    .catch(next)
     .catch(() => {
       return res.status(400).send("Category not created!");
     });
 });
 
-server.delete("products/category/:id", (req, res, next) => {
+server.delete("/:id", (req, res, next) => {
   const { id } = req.params;
   Categories.destroy({
     where: { id: id },
@@ -23,23 +22,30 @@ server.delete("products/category/:id", (req, res, next) => {
     .then(() => {
       res.send("Category deleted");
     })
-    .catch(next)
     .catch(() => {
       return res.status(400).send("Category not found!");
     });
 });
 
-server.put('/products/category/:id', (req, res, next) => {
-  const {id} = req.params;
-   const {name, description}= req.body /* <--- elemento del body que utilizaremos para el update */
+server.put("/:id", (req, res, next) => {
+  const { id } = req.params;
+  const {
+    name,
+    description,
+  } = req.body; /* <--- elemento del body que utilizaremos para el update */
   Categories.update(
-     {name:name, description:description}, /* <----los atributos que queremos updatear */
-    {where:{id:id}}
-  ).then(()=>{
-    res.status(202).send('Element updated')
-  }).catch(() => {
-    return res.status(400).send("Category not found!");
-  });
+    {
+      name: name,
+      description: description,
+    } /* <----los atributos que queremos updatear */,
+    { where: { id: id } }
+  )
+    .then(() => {
+      res.status(202).send("Element updated");
+    })
+    .catch(() => {
+      return res.status(400).send("Category not found!");
+    });
 });
 
 module.exports = server;
--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,87 +1,132 @@
-const server = require('express').Router();
-const { Sequelize } = require('sequelize');
-const { Product, Categories } = require('../db.js');
+const server = require("express").Router();
+const { Sequelize, where } = require("sequelize");
+const { Product, Categories } = require("../db.js");
 const Op = Sequelize.Op;
 
 // Checking for a match in database and create the product.
 // Of succeed, it create the product, return 201 status and product information, else return error and status 400!
-server.post('/', (req, res, next) => {
-	const {name, description, price, stock} = req.body;
-	Product.findOrCreate({
-		where:{
-				name: name,
-				description: description,
-				price: price,
-				stock: stock,
-				image: 'don\'t forget to make it'
-		}
-	})
-	.then(product => {
-		res.status(201).send(product);
-	})
-	.catch(()=>{
-		return res.status(400).send('Product not created!');
-	})
+server.post("/", (req, res, next) => {
+  const { name, description, price, stock } = req.body;
+  Product.findOrCreate({
+    where: {
+      name: name,
+      description: description,
+      price: price,
+      stock: stock,
+      image: "don't forget to make it",
+    },
+  })
+    .then((product) => {
+      res.status(201).send(product);
+    })
+    .catch(() => {
+      return res.status(400).send("Product not created!");
+    });
 });
 //Simply requesting for all products with findAll and catching possible errors.
-server.get('/', (req, res)=>{
-	Product.findAll()
-		.then(products=>{
-			res.send(products)
-		})
-		.catch(()=>{
-			return res.status(400).send("Couldn't find products!");
-		})
-})
+server.get("/", (req, res) => {
+  Product.findAll()
+    .then((products) => {
+      res.send(products);
+    })
+    .catch(() => {
+      return res.status(400).send("Couldn't find products!");
+    });
+});
 
 // Crear ruta que retorne productos segun el keyword de búsqueda
 // GET /search?query={valor}
 // Retorna todos los productos que tengan {valor} en su nombre o descripción.
-server.get('/search', (req, res)=>{
-	const producto = req.query.valor;
-	Product.findAndCountAll({
-		where: {
-			[Op.or]:[
-				{
-					name: { 
-						[Op.like]: `%${producto}%` 
-					}
-				}, 
-				{
-					description: { 
-						[Op.like]: `%${producto}%` 
-					}
-				}
-			]
-		}
-	})
-	.then((product)=>{
-		res.send(product)
-	})
-	.catch(()=>{
-		return res.status(400).send("Couldn't find any products!");
-	})
-})
+server.get("/search", (req, res) => {
+  const producto = req.query.valor;
+  Product.findAndCountAll({
+    where: {
+      [Op.or]: [
+        {
+          name: {
+            [Op.like]: `%${producto}%`,
+          },
+        },
+        {
+          description: {
+            [Op.like]: `%${producto}%`,
+          },
+        },
+      ],
+    },
+  })
+    .then((product) => {
+      res.send(product);
+    })
+    .catch(() => {
+      return res.status(400).send("Couldn't find any products!");
+    });
+});
 
 // GET /products/:id
 // Retorna un objeto de tipo producto con todos sus datos. (Incluidas las categorías e imagenes).
-server.get('/:id', (req, res)=>{
-	const id = req.params.id;
-	Product.findOne({
-		where: {
-			id: id,
-		},
-		include: {
-			model: Categories
-		}
-	})
-		.then(product=>{
-				res.json(product)
-		})
-		.catch(()=>{
-			return res.status(400).send("Couldn't find the product!");
-		})
-})
+server.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Product.findOne({
+    where: {
+      id: id,
+    },
+    include: {
+      model: Categories,
+    },
+  })
+    .then((product) => {
+      res.json(product);
+    })
+    .catch(() => {
+      return res.status(400).send("Couldn't find the product!");
+    });
+});
 
+server.post("/:idProducto/category/:idCategoria", (req, res, next) => {
+  //add the category to the product
+
+  const { idProducto, idCategoria } = req.params;
+  const { description, name } = req.body;
+  Categories.findOrCreate({
+    where: { id: idCategoria, name: name, description: description },
+    include: {
+      model: Product,
+      where: {
+        id: idProducto,
+      },
+    },
+  })
+    .then((product) => {
+      res.status(201).send(product);
+    })
+    .catch(() => {
+      return res.status(400).send("Product not created!");
+    });
+});
+
+server.delete("/:idProducto/category/:idCategoria", (req, res, next) => {
+  /* this one is for deleting the product category;) */
+  const { idProducto, idCategoria } = req.params;
+  Product.destroy({
+    where: {
+      id: idCategoria,
+      include: {
+        model: Categories,
+        where: {
+          id: idProducto,
+        },
+      },
+    },
+  })
+    .then((result) => {
+      if (result) {
+        return res.send("Category deleted");
+      }
+      return res.status(400).send("Category not found!");
+    })
+    .catch(next);
+  //.catch((err) => next({status:404, message:'Not found'}))  *******  <-----ignore this, its for future references *******
+});
 
 module.exports = server;
