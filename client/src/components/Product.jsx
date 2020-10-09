9b281d846866d8d93d74f790d8ebd9879b6732a1
Franco <you@example.com>
2020-10-09 13:03:11 -0300

    componente Product

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -2,45 +2,49 @@ import React from "react";
 import { useState,useEffect } from "react";
 import axios from "axios"
 import data from "../data"
-import "../components/Product.css";
+import {Card, Button} from "react-bootstrap"
+
+
 
 
 export default function Product(props){
 //    console.log(props)
-     let mapeo = data.products.filter(ele => ele.id == props.match.params.id )
-    
-    
+   let mapeo = data.products.filter(ele => ele.id == props.match.params.id )
+  
+  
 
-    const [product,setProduct]=useState({});
-    function mostrarProducto(prod){
-    axios.get("http://localhost:4000/products/" + mapeo[0].id)
-    .then(res=>
-        res.data
-    )
-    .then(res=>
-        setProduct(res)
-    )
-    }
+  const [product,setProduct]=useState({});
+  
+  function mostrarProducto(prod){
+  axios.get("http://localhost:4000/products/" + mapeo[0].id)
+  .then(res=>
+    res.data
+  )
+  .then(res=>
+    setProduct(res)
+  )
+
+  }
+  
+  
+  mostrarProducto()
     
-    
-    mostrarProducto()
-        
+  
+  return( 
+     <div className="text">
       
-    return( 
-         <div>
-{/*             
-        //     <div className="Name">
-        //      <h1>{mapeo.name}</h1>
-        //      {console.log(mapeo.name)}
-        //      </div>
-        //      <div className="Propiedades">
-        //      <h5>{mapeo[0].description}</h5>
-        //      <h7>{mapeo[0].price}</h7> 
-        //      <h7>{mapeo[0].stock}</h7> 
-        //      </div> */}
-
-          <h1>{product.image}</h1>
-            
-         </div>
-    )
+      <Card className="text-center">
+        
+      <Card.Body>
+    <Card.Title>{product.name}</Card.Title>
+      <Card.Text>
+      {product.description}.
+       </Card.Text>
+       <Button variant="primary">Buy</Button>
+       </Card.Body>
+       <Card.Footer className="text">Stock:{product.stock}</Card.Footer>
+       <Card.Footer className="text">Price:{product.price}</Card.Footer>
+      </Card>
+     </div>
+  )
 }
