c108f8282e955b586eeaae304f456aed4d75f1c5
SantiagoLesait <68793844+SantiagoLesait@users.noreply.github.com>
2020-10-12 21:53:20 -0300

    Update Cart.js

--- api/src/models/Cart.js
+++ api/src/models/Cart.js
@@ -10,6 +10,19 @@ module.exports = (sequelize) => {
       primaryKey: true,
       autoIncrement: true
     },
+    totalPrice: {
+        type: DataTypes.DECIMAL(9, 2),
+        allowNull: false,
+        validate: {     
+          isDecimal: true,
+          min: {
+            args: [0]       
+          },
+          max: {
+            args: [999999999]           
+          }
+        }
+      },
       state: {
         type: DataTypes.ENUM({
             values: ['Cart', 'Creada', 'Procesando', 'Cancelada', 'Completa']
