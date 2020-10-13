3127499f9314f948fb567fd1c73c6e3dcf4bfc42
Santiago Barrios <santiagobarrios91@gmail.com>
2020-10-13 20:54:37 -0300

    S44-Crear-ruta-que-retorne-todas-las-órdenes

---
+++ api/src/routes/orders.js
@@ -0,0 +1,18 @@
+const server = require("express").Router();
+const { Order } = require("../db.js");
+
+server.get('/orders', (req, res, next) => {
+const state = req.query.status;   
+  Order.findAll({
+    where: {
+      state: state
+    }
+  })
+  .then((orders) => {
+    res.status(200).json(orders);
+  }).catch((err)=>{
+    return res.send({data: err}).status(400)
+  })
+})
+
+module.exports = server;
