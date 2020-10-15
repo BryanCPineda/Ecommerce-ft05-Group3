ac50f04cfa55bc48ce9c416fcae2aaf989cdfac9
Franco <you@example.com>
2020-10-15 19:28:34 -0300

    anada el app

--- client/src/App.js
+++ client/src/App.js
@@ -9,13 +9,11 @@ import axios from "axios";
 import Catalogo from "./components/Catalogo";
 import CrudShow from "./components/CrudProducts/CrudShow";
 import AdminProducts from "./components/AdminProducts/AdminProducts";
-import LandingPage from './components/LandingPage';
-import PrductsMati from './components/ProductsMati';
-import Footer from './components/Footer';
-import NavbarAdmin from './components/NavbarAdmin';
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
-
+import LandingPage from "./components/LandingPage";
+import PrductsMati from "./components/ProductsMati";
+import Footer from "./components/Footer";
+import NavbarAdmin from "./components/NavbarAdmin";
+import Cart from "./components/Cart/Cart";
 
 
 import store from "./store";
@@ -25,24 +23,6 @@ function App() {
   return (
     <div>
       <Provider store={store}>
-
-      <Router> 
-       
-        <Route path="/user" render={() => <SearchBar handleSubmit={handleSubmit} handleChange={handleChange} />}/>
-        <Route path="/admin" component={NavbarAdmin} />
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/user/catalogo"
-          render={() => <Catalogo />}
-        />
-        <Route exact path="/admin/categories" component={FormCategories} />
-        {/* <Route exact path="/admin/producto" component={CrudShow} /> */}
-        <Route exact path="/admin/product" component={AdminProducts} />
-        <Route exact path="/user/product/:id" component={PrductsMati} />
-        <Route path="/user" component={Footer} />
-        <Route path="/SignIn" component={Login} />
-        <Route path="/SignUp" component={SignUp} />
-      </Router>
-
         <Router>
           <Route path="/user" render={() => <SearchBar />} />
           <Route path="/admin" component={NavbarAdmin} />
@@ -55,7 +35,6 @@ function App() {
           <Route path="/user" component={Footer} />
           <Route path="/user/cart" component={Cart} />
         </Router>
-
 
       </Provider>
     </div>
