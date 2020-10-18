24ae9f7a416f0ebfae5a8a75e4fd32ca8c494e9a
Cinthia Daira Pardos <cin@pop-os.localdomain>
2020-10-18 19:58:24 -0300

    carrito vacio

--- client/src/components/Cart/Order.jsx
+++ client/src/components/Cart/Order.jsx
@@ -7,24 +7,23 @@ import {
   Button,
   Card,
   Modal,
-  Image
+  Image,
 } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Axios from "axios";
 import DropD from "./dropdown.jsx";
-import ProductCard from '../ProductCard'
-import { connect } from 'react-redux';
+import ProductCard from "../ProductCard";
+import { connect } from "react-redux";
 import { getAllUserOrders } from "../../actions/cartActions";
-
+import "./Order.css";
 
 const Order = (props) => {
-  const idUser=1
-  
-  console.log('las orderlinesstate',props.allOrderLines)
+  const idUser = 1;
+
+  console.log("las orderlinesstate", props.allOrderLines);
   useEffect(() => {
     props.getAllUserOrders(idUser);
   }, []);
-
 
   return (
     <Row>
@@ -38,45 +37,53 @@ const Order = (props) => {
           variant="light"
           className="table-container"
         >
-             {props.allOrderState ? props.allOrderState.map((ele, index) => (
-                  <thead>    
-                  <tr>
-                    <th><Image src={ele.images[0]} alt='File Image of the Product'/></th>
-                  <th>{ele.name}</th>
-    
-                <th>Price: ${ele.orderline.price}</th>
+          {props.allOrderState ? (
+            props.allOrderState.map((ele, index) => (
+              <thead>
+                <tr>
                   <th>
-                    <DropD quantity={ele.orderline.quantity}/>
+                    <Image
+                      src={ele.images[0]}
+                      alt="File Image of the Product"
+                    />
+                  </th>
+                  <th>{ele.name}</th>
+
+                  <th>Price: ${ele.orderline.price}</th>
+                  <th>
+                    <DropD quantity={ele.orderline.quantity} />
                   </th>
                   <Button size="sm" className="cart-button">
                     Eliminar
                   </Button>
                 </tr>
               </thead>
-              )) : <div>Default Markup</div>}
-                
-              
-              
-          
+            ))
+          ) : (
+            <div>
+              <p>
+                <img src="../images/shopping_Sad-512.png" alt="sad cart"></img>
+                <h3> Your cart is empty!</h3> <br></br> Add something to make me
+                happy :)
+              </p>
+            </div>
+          )}
         </Table>
       </Col>
     </Row>
   );
 };
 
-
 const mapStateToProps = (state) => {
   return {
     allOrderState: state.cartReducer.allOrderLines,
-  
-  }
-}
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
     getAllUserOrders: (idUser) => dispatch(getAllUserOrders(idUser)),
-  }
-}
+  };
+};
 
-export default connect( mapStateToProps, mapDispatchToProps )(Order);
-
+export default connect(mapStateToProps, mapDispatchToProps)(Order);
