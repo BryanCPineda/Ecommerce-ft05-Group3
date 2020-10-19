5a0fa096fd2c2147df700aaec29b16a6fef41e06
Cinthia Daira Pardos <cin@pop-os.localdomain>
2020-10-19 14:01:10 -0300

    finals

--- client/src/components/CartUsage/CartUse.jsx
+++ client/src/components/CartUsage/CartUse.jsx
@@ -1,163 +1,158 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import { FiPlus } from "react-icons/fi";
-import {Container, Row, Col, Modal, Button} from 'react-bootstrap'
-import {IoMdTrash, IoMdPhotos, IoIosCart} from 'react-icons/io'
-import NumberFormat from "react-number-format"
-import OrderUse from './OrderUse'
-import swal from 'sweetalert'
-import { Redirect } from 'react-router-dom'
-import './CartUse.css'
+import { Container, Row, Col, Modal, Button } from "react-bootstrap";
+import { IoMdTrash, IoMdPhotos, IoIosCart } from "react-icons/io";
+import NumberFormat from "react-number-format";
+import OrderUse from "./OrderUse";
+import swal from "sweetalert";
+import { Redirect } from "react-router-dom";
+import "./CartUse.css";
 
 //-------------- Redux ------------------------
-import { connect } from 'react-redux'
-import { getOrder, cambioEstadoCarrito, vaciarCarrito, quitarItemCarrito} from '../../actions/order'
-import { getProducts, updateProduct } from "../../actions/product"
+import { connect } from "react-redux";
+import {
+  getOrder,
+  cambioEstadoCarrito,
+  vaciarCarrito,
+  quitarItemCarrito,
+} from "../../actions/order";
+import { getProducts, updateProduct } from "../../actions/product";
 
+const Cart = ({
+  order,
+  getOrder,
+  products,
+  getProducts,
+  updateProduct,
+  cambioEstadoCarrito,
+  vaciarCarrito,
+  quitarItemCarrito,
+}) => {
+  //const [state, setState] = useState({
+  //products: products
+  //})
+  console.log("-------------", order);
+  const [total, setTotal] = useState(0);
 
+  // ------------------redireccionar --------------
+  const [stateRedirect, setRedirect] = useState({ redirect: null });
 
-const Cart = ({order, getOrder, products, getProducts, updateProduct, cambioEstadoCarrito, vaciarCarrito, quitarItemCarrito}) => {
-    
-      //const [state, setState] = useState({
-      //products: products
-      //})
-      console.log('-------------', order)
-      const [total, setTotal] = useState(0)
+  // ------------------redireccionar --------------
 
-      // ------------------redireccionar --------------
-      const [stateRedirect, setRedirect] = useState({ redirect: null })
+  let prod = [];
 
-      // ------------------redireccionar --------------
+  let totalCostTmp = 0;
 
-    let prod = []
+  const [totalCost, setTotalCost] = useState(0);
 
-    let totalCostTmp=0;
+  useEffect(() => {
+    getOrder();
+  }, [totalCost]);
 
-    const [totalCost, setTotalCost] = useState(0)
+  //useEffect(() => {
+  //  setState({
+  //    products: order.product
+  //    })
+  // console.log('state luego de borrar',state)
+  //}, [quitarItemCarrito])
 
-    useEffect(() => {
-        getOrder()
-      }, [totalCost])
-
-    
-
-
-      //useEffect(() => {
-      //  setState({
-      //    products: order.product
-      //    })
-      // console.log('state luego de borrar',state)
-      //}, [quitarItemCarrito])
-    
-    const quantityChange = (e, id) =>{
-        let cantCambiada = e
-        prod = order.product
-        prod ? prod.forEach( e => {
-          if (e.orderline.id === id){
-            e.stock = e.stock + e.orderline.quantity - cantCambiada
-            e.orderline.quantity = cantCambiada
-          }}
-        ) : console.log('nada')
-
-
-        if(prod){
-          prod.forEach(element => {
-              totalCostTmp = totalCostTmp + parseInt(element.price)
-          })
-        }setTotalCost(totalCostTmp)
-        
-       
-        
-        
-    }
-    console.log(totalCostTmp)
-    console.log(totalCost)
-
-
-    const handleDelete = (id) =>{
-      swal({
-        title: "Are you sure?",
-        text: "You will delete this item from your cart!",
-        icon: "warning",
-        buttons: true,
-        dangerMode: true,
-      })
-      .then((willDelete) => {
-        if (willDelete) {
-          quitarItemCarrito(id)
-
-          
-          swal("Your cart is Empty!", {
-            icon: "success",
-          }).then(() =>  {
-                      
-
-          })
-  
-      } } )
-       
-
-    }
-
-    const handleFinCompra =() =>{
-
-      let prodEnviar = []
-        products.map (e => {
-          prodEnviar = {
-            name: e.name,
-            description: e.description,
-            price: e.price,
-            stock: e.stock,
-            categories:'',
-            images: ''
+  const quantityChange = (e, id) => {
+    let cantCambiada = e;
+    prod = order.product;
+    prod
+      ? prod.forEach((e) => {
+          if (e.orderline.id === id) {
+            e.stock = e.stock + e.orderline.quantity - cantCambiada;
+            e.orderline.quantity = cantCambiada;
           }
-          updateProduct(e.id, prodEnviar)}
-        ) 
-      cambioEstadoCarrito(order.orderId, 'Created')
-      swal("Order Created!", {
-        icon: "success",
-      }).then(() => {
-          setRedirect({ redirect: "/user/catalogo" });
-        }
-      )
-        
+        })
+      : console.log("nada");
+
+    if (prod) {
+      prod.forEach((element) => {
+        totalCostTmp = totalCostTmp + parseInt(element.price);
+      });
+    }
+    setTotalCost(totalCostTmp);
+  };
+  console.log(totalCostTmp);
+  console.log(totalCost);
+
+  const handleDelete = (id) => {
+    swal({
+      title: "Are you sure?",
+      text: "You will delete this item from your cart!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        quitarItemCarrito(id);
+
+        swal("Your cart is Empty!", {
+          icon: "success",
+        }).then(() => {});
       }
+    });
+  };
 
-    const handleVaciarCarrito = () =>{
-      swal({
-        title: "Are you sure?",
-        text: "You will empty your cart!",
-        icon: "warning",
-        buttons: true,
-        dangerMode: true,
-      })
-      .then((willDelete) => {
-        if (willDelete) {
-          vaciarCarrito()
+  const handleFinCompra = () => {
+    let prodEnviar = [];
+    products.map((e) => {
+      prodEnviar = {
+        name: e.name,
+        description: e.description,
+        price: e.price,
+        stock: e.stock,
+        categories: "",
+        images: "",
+      };
+      updateProduct(e.id, prodEnviar);
+    });
+    cambioEstadoCarrito(order.orderId, "Created");
+    swal("Order Created!", {
+      icon: "success",
+    }).then(() => {
+      setRedirect({ redirect: "/user/catalogo" });
+    });
+  };
 
-          swal("Your cart is Empty!", {
-            icon: "success",
-          }).then(() => {
+  const handleVaciarCarrito = () => {
+    swal({
+      title: "Are you sure?",
+      text: "You will empty your cart!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        vaciarCarrito();
+
+        swal("Your cart is Empty!", {
+          icon: "success",
+        }).then(() => {
           setRedirect({ redirect: "/user/catalogo" });
-            })
-  
-      } } )
-  
-    }
-    if (stateRedirect.redirect) {
-            return <Redirect to={stateRedirect.redirect} />
-          }
-      
-  return ( 
-    
-    <Container className="container-cart-use">
-      <Row className='m-3 d-none d-md-block cart-text '>
-        <Col className=" text-center py-2" style={{color: 'white'}}> 
-        My Cart <IoIosCart />
-        </Col> 
-      </Row>
-        <Row className="m-3 d-none d-md-block">
-          <Col>
-            {/* <Row className="bg-light text-center py-2"> */}
+        });
+      }
+    });
+  };
+  if (stateRedirect.redirect) {
+    return <Redirect to={stateRedirect.redirect} />;
+  }
+
+  return (
+    <Row>
+      <Col xs={2}></Col>
+      <Col>
+        <Container className="container-cart-use">
+          <Row className="m-3 d-none d-md-block cart-text ">
+            <Col className=" text-center py-2" style={{ color: "white" }}>
+              My Cart <IoIosCart />
+            </Col>
+          </Row>
+          <Row className="m-3 d-none d-md-block">
+            <Col>
+              {/* <Row className="bg-light text-center py-2"> */}
               {/* <Col xs={3} md={2}>
                 <span className="h3">
                   <IoMdPhotos />
@@ -182,31 +177,39 @@ const Cart = ({order, getOrder, products, getProducts, updateProduct, cambioEsta
                   </Col>
                 </Row>
               </Col> */}
-            {/* </Row> */}
-          </Col>
-        </Row>
-        <Row className="m-3 d-none d-md-block">
-          <Col>
-            <Row className="bg-light text-center py-2  ">
-              <Col className="mx-3">
-                  
-                  {products ? products.map (e => 
-                  <OrderUse orderline={e} quantityChange={quantityChange} handleDelete={handleDelete}/>) :
-                  <div>
-                  <p>
-                    <img src="../images/shopping_Sad-512.png" alt="sad cart"></img>
-                    <h3> Your cart is empty!</h3> <br></br> Add something to make me
-                    happy :)
-                  </p>
-                </div>}
-                
-              </Col>
-            </Row>
+              {/* </Row> */}
             </Col>
-        </Row>
+          </Row>
+          <Row className="m-3 d-none d-md-block">
+            <Col>
+              <Row className="bg-light text-center py-2  ">
+                <Col className="mx-3">
+                  {products ? (
+                    products.map((e) => (
+                      <OrderUse
+                        orderline={e}
+                        quantityChange={quantityChange}
+                        handleDelete={handleDelete}
+                      />
+                    ))
+                  ) : (
+                    <div>
+                      <p>
+                        <img
+                          src="../images/shopping_Sad-512.png"
+                          alt="sad cart"
+                        ></img>
+                        <h3> Your cart is empty!</h3> <br></br> Add something to
+                        make me happy :)
+                      </p>
+                    </div>
+                  )}
+                </Col>
+              </Row>
+            </Col>
+          </Row>
 
           <Row className="mx-3 text-center">
-       
             <Col xs={6} className="text-center bg-light p-3 ml-auto">
               <h4 className="mb-4 ">
                 Total:
@@ -221,46 +224,48 @@ const Cart = ({order, getOrder, products, getProducts, updateProduct, cambioEsta
               <Button
                 className="btn  boton"
                 onClick={handleFinCompra}
-                style={{backgroundColor: '#8a2be2', color: 'white', border: 'none'}}
+                style={{
+                  backgroundColor: "#8a2be2",
+                  color: "white",
+                  border: "none",
+                }}
               >
                 Finalize Purchase
-                </Button>
+              </Button>
               <button
                 className="btn  boton"
-                style={{backgroundColor: '#8a2be2', color: 'white'}}
+                style={{ backgroundColor: "#8a2be2", color: "white" }}
                 onClick={handleVaciarCarrito}
               >
                 Empty Cart
               </button>
             </Col>
           </Row>
-        {/* )} */}
-      </Container>
-    );
-}
+          {/* )} */}
+        </Container>
+      </Col>
+      <Col xs={2}></Col>
+    </Row>
+  );
+};
 
 function mapStateToProps(state) {
-    return {
-            order: state.orderReducer.order,
-            products: state.orderReducer.products,
-          
-    }
+  return {
+    order: state.orderReducer.order,
+    products: state.orderReducer.products,
+  };
 }
-
 
 function mapDispatchToProps(dispatch) {
-    return {
-            getOrder: () => dispatch(getOrder()),
-            getProducts: () => dispatch(getProducts()),
-            cambioEstadoCarrito: (id, status) => dispatch(cambioEstadoCarrito(id, status)),
-            updateProduct: (id, prod) => dispatch(updateProduct(id, prod) ),
-            vaciarCarrito: () => dispatch(vaciarCarrito()),
-            quitarItemCarrito: (id) => dispatch(quitarItemCarrito(id))
-          
-    }
+  return {
+    getOrder: () => dispatch(getOrder()),
+    getProducts: () => dispatch(getProducts()),
+    cambioEstadoCarrito: (id, status) =>
+      dispatch(cambioEstadoCarrito(id, status)),
+    updateProduct: (id, prod) => dispatch(updateProduct(id, prod)),
+    vaciarCarrito: () => dispatch(vaciarCarrito()),
+    quitarItemCarrito: (id) => dispatch(quitarItemCarrito(id)),
+  };
 }
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Cart);
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
--- client/src/components/NavbarGeneral.jsx
+++ client/src/components/NavbarGeneral.jsx
@@ -1,68 +1,79 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import './NavbarGeneral.css'
-import SearchBar from './SearchBar/SearchBar'
-import SignUp from './Users/userRegister'
-import { Button, Row, Col } from 'react-bootstrap';
-import { IoIosCart } from 'react-icons/io';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavbarGeneral.css";
+import SearchBar from "./SearchBar/SearchBar";
+import SignUp from "./Users/userRegister";
+import { Button, Row, Col } from "react-bootstrap";
+import { IoIosCart } from "react-icons/io";
 
 function NavbarGeneral() {
+  // window.addEventListener('scroll', function () {
+  // 	let header = document.querySelector('header');
+  // 	let windowPosition = window.scrollY > 0;
+  // 	header.classList.toggle('scrolling-active', windowPosition);
+  // })
 
-	// window.addEventListener('scroll', function () {
-	// 	let header = document.querySelector('header');
-	// 	let windowPosition = window.scrollY > 0;
-	// 	header.classList.toggle('scrolling-active', windowPosition);
-	// })
+  return (
+    // 		<div style={{backgroundColor: 'blue'}} className="navegacion-general">
+    // 			<head>
+    //     {/* <meta charset="UTF-8">
+    //     <meta name="viewport"
+    //           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    //     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    //     <title>Document</title>
+    //     <link rel="stylesheet" href="styles.css"> */}
+    // </head>
+    // <body className='fontuse'>
+    //     <header>
+    //         <div class="container">
+    <nav className="background-al-nav-general">
+      <Row class="">
+        <Col xs={2}></Col>
+        <Col className="d-flex mt-5">
+          <Link to="/user/catalogo">
+            <div class="logo">
+              <img
+                class="image-brand"
+                src={"/images/brand4.png"}
+                alt="logo"
+              ></img>
+            </div>
+          </Link>
+          <div class="searchbar-navbar">
+            <SearchBar />
+          </div>
+          <ul class="nav-list">
+            <li>
+              <Link to="/admin" class="nav-link admin-icono-navbar">
+                Admin
+              </Link>
+            </li>
+            <li>
+              <Link to="/user/cart" class="nav-link cart-icono-navbar">
+                Cart{" "}
+                <span style={{ fontSize: "35px" }}>
+                  <IoIosCart />
+                </span>
+              </Link>
+            </li>
+          </ul>
+          <span class="nav-cta nav-sign">
+            <SignUp />{" "}
+          </span>
+        </Col>
+        <Col xs={2}></Col>
 
-	return (
-// 		<div style={{backgroundColor: 'blue'}} className="navegacion-general">
-// 			<head>
-//     {/* <meta charset="UTF-8">
-//     <meta name="viewport"
-//           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//     <title>Document</title>
-//     <link rel="stylesheet" href="styles.css"> */}
-// </head>
-// <body className='fontuse'>
-//     <header>
-//         <div class="container">
-<nav className="background-al-nav-general">
-            <Row class="" >
-				<Col xs={2}></Col>
-				<Col className="d-flex mt-5">
-				<Link to="/user/catalogo">
-				<div class="logo" >
-					<img class='image-brand' src={"/images/brand4.png"} alt="logo"></img>
-				</div>				
-				</Link>
-				<div class='searchbar-navbar'><SearchBar /></div>
-                <ul class="nav-list">
-					
-					<li>
-						<Link to ="/admin"class="nav-link admin-icono-navbar">Admin</Link>
-                    </li>
-					<li>
-						<Link to ="/user/cart"class="nav-link cart-icono-navbar">Cart <span style={{fontSize: '35px'}}><IoIosCart /></span></Link>
-                    </li>
-                </ul>
-				<span class="nav-cta"><SignUp /> </span>
-				</Col>
-				<Col xs={2}></Col>
-				
-                {/* <a href="#" id="nav-cta">Login</a> */}
-            </Row>
-			</nav>
-//         </div>
-//     </header>
-// </body>
-// 		</div>
-	)
+        {/* <a href="#" id="nav-cta">Login</a> */}
+      </Row>
+    </nav>
+    //         </div>
+    //     </header>
+    // </body>
+    // 		</div>
+  );
 }
 
-export default NavbarGeneral
-
-
+export default NavbarGeneral;
 
 // import React from 'react'
 // import {Link} from 'react-router-dom'
@@ -71,7 +82,7 @@ export default NavbarGeneral
 
 // function NavbarGeneral() {
 // 	return (
-		
+
 // 		<header class="header">
 // 		<nav class="navbar navbar-expand-lg fixed-top py-3">
 // 			<Link to="/user/catalogo">
@@ -81,7 +92,7 @@ export default NavbarGeneral
 // 			</Link>
 // 			<div class="container" ><a href="#" class="navbar-brand text-uppercase font-weight-bold">Ciber Fitness</a>
 // 				<button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right"><i class="fa fa-bars"></i></button>
-	
+
 // 				<div id="navbarSupportedContent" class="collapse navbar-collapse" >
 // 					<ul class="navbar-nav ml-auto" className='searchbar'>
 // 						<li class="nav-item"><SearchBar /></li>
@@ -95,12 +106,8 @@ export default NavbarGeneral
 // 			</div>
 // 		</nav>
 // 		</header>
-	
-		
+
 // 	)
 // }
 
 // export default NavbarGeneral
-
-
-
