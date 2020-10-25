91a484e961b4ee77b3f20a0a801c468f52d757c2
larts85 <larts85@yahoo.com>
2020-10-25 19:59:47 -0300

    to bring the master

--- api/src/routes/reviews.js
+++ api/src/routes/reviews.js
@@ -1,26 +1,131 @@
 const server = require("express").Router();
-const { Reviews, Product, Users } = require("../db.js");
+const { Sequelize } = require("sequelize");
+const { Reviews, Orderline, Users, Order, Product } = require("../db.js");
 
 
-server.get("/product/:id/review", async (req, res) => {
+server.get("/product/:id/review", (req, res) => {
   const id = req.params.id;
+  Reviews.findAndCountAll({
+    where:{
+      productId:id,
+    },
+  })
+  .then((reviews) => {
+    let usersIds = reviews.rows.map(e => e.dataValues.userId)
+    let infoUsers=[];
+    usersIds.map(id => {
+      Users.findOne({
+        where:{
+          id:id,
+        },
+        attributes: {
+          exclude: ['password']
+        }
+      })
+      .then((user)=>{
+        infoUsers.push(user.dataValues)
+      })
+      .then(() =>{
+        if(infoUsers.length === usersIds.length){
+          let response = {
+            reviews: reviews,
+            users: infoUsers
+          }
+          res.send(response).status(200);
+        }
+      })
+    })
+  })
+  .catch((err) => {
+    return res.send({ data: err }).status(400);
+  });
+})
+// This function brings necesary data for the ReviewCard component
+server.get('/:userId/products', async (req, res)=>{
   try {
+    const userId = req.params.userId;
+    const orders = await Order.findAll({
+      where: {
+        userId: userId
+      },
+    })
+    let orderIds = orders.map(order=>order.dataValues.id);
+    console.log('orders', orders)
+    let userOrderlines = await Orderline.findAndCountAll({
+      where: {
+        orderId: orderIds
+      }  
+    })
+    let productIds = userOrderlines.rows.map(orderLine=>orderLine.dataValues.productId)
+    // let products = await Product.findAndCountAll({
+    //   where: {
+    //     id: productIds
+    //   },
+    // })
     let reviews = await Reviews.findAndCountAll({
-      where:{
-        productId: id,
-      }
+      where: {
+        productId: productIds,
+        userId: userId
+      },
+      // include: {
+      //   model: Product,
+      //   where: {
+      //     id: Sequelize.col('reviews.productId')
+      //   }
+      // }
     })
-    let userIds = await Reviews.map(review=>{
-      console.log('REVIEWS', userIds)
-      return review.row[0].dataValues.userId
-    })
-    res.status(200).send(reviews);
-    
-  } catch (err) {
+    let reviewQualification = reviews.rows.map(e => e.dataValues.qualification)
+    let reviewDescription = reviews.rows.map(e => e.dataValues.description)
+    let productNames = products.rows.map(e => e.dataValues.name)
+    let productId = products.rows.map(e => e.dataValues.id)
+    let productPrices = products.rows.map(e => e.dataValues.price)
+
+    // console.log('reviewQualification', reviewQualification)
+    // console.log('reviewDescription', reviewDescription)
+    // console.log('productIds', productId)
+    // console.log('productNames', productNames)
+    // console.log('productPrices', productPrices)
+    res.send(reviews)
+  } 
+  catch (err) {
     return res.send({ data: err }).status(400);
   }
 })
-
+// server.get('/:userId/products', async (req, res)=>{
+//   try {
+//     const userId = req.params.userId;
+//     const orders = await Reviews.findAll({
+//       where: {
+//         userId: userId
+//       },
+//       attributes: ['qualification', 'description'],
+//       include: {
+//         model: Order,
+//         where: {
+//           userId: userId
+//         },
+//         include: {
+//           model: Orderline,
+//           where: {
+//             orderId: Order.id
+//           },
+//           attributes: ['name', 'price'],
+//           include: {
+//             model: Product,
+//             where: {
+//               productId: Orderline.productId
+//             }
+//           }
+//         }
+//       }
+//     })
+//     // console.log('reviews', reviews)
+//     res.send(orders)
+//   } 
+//   catch (err) {
+//     return res.send({ data: err }).status(400);
+//   }
+// })
 server.get('/product/:id/oneStarReviews', async (req, res)=>{
   try {
     const {id} = req.params;
--- client/src/components/Reviews/ReviewCard.jsx
+++ client/src/components/Reviews/ReviewCard.jsx
@@ -1,18 +1,93 @@
 import React from 'react'
-import { Modal } from 'react-bootstrap'
+import { Col, Modal, Row, Table } from 'react-bootstrap'
+import store from '../../store'
+import AddReview from './AddReview';
+import EditReview from './AddReview';
+import DeleteReview from './AddReview';
 
 function ReviewCard() {
+  const[show, setShow] = useState(false);
+
+  // const handleOnclick = (e) => {
+  //   e.preventDefault();
+  //   setShow(true);
+  // }
+  const state = store.getState();
+  const orders = state.ordersReducer.orders;
+  const reviews = state.reviewsReducer.reviews;
+  const products = state.productsReducer.reviews;
+  console.log('ORDERS', orders);
+  console.log('REVIEWS', reviews);
+  console.log('PRODUCTS', products);
+
+  const array = [];
+  const p = products.map(product=>{
+    array.push({
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      qualification: qualification = ()=>{reviews.find(review.productId === product.id)}
+    })
+  })
+  let createMyCustomArray = () => {
+    for (let i = 0; i < array.length; i++) {
+      
+    }
+    return
+  }
+
   return (
     <React.Fragment>
-      <Modal>
-        <Modal.Header>
-          
+      <Modal size='xl'>
+        <Modal.Header
+          style={{ backgroundColor: "#8a2be2", color: "white" }}
+          closeButton={true} 
+          closeLabel={'Close'}
+        >
+          Your products reviews
         </Modal.Header>
         <Modal.Body>
-
+        <Table striped bordered hover className="table table-ligth table-sm table-bordered table-hover">
+              <thead>
+                <tr className="text-center">
+                  <th>Price</th>
+                  <th>Product</th>
+                  <th>Stars</th>
+                  <th>Description</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {order &&
+                  order.map((order, index) => {
+                    return (
+                      <tr key={index} className="text-center">
+                        <td>{order.product[0] && order.product[0]}</td>
+                        <td>{}</td>
+                        <td>{}</td>
+                        <td>{}</td>
+                        <td>{<EditReview/>}{<DeleteReview/>}</td>
+                      </tr>
+                    );
+                  })
+                }
+              </tbody>
+            </Table>
         </Modal.Body>
         <Modal.Footer>
-          
+          <Row>
+            <Col lg='5'>
+              <Button
+                  onClick={()=>setShow(false)}
+                  className="button-register mt-1"
+              >Close
+              </Button>
+            </Col>
+            <Col lg='7'></Col>
+            <Col lg='3'>
+              <AddReview/>
+            </Col>
+          </Row>
         </Modal.Footer>
       </Modal>
     </React.Fragment>
